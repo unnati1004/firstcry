6fb2248ba7d3112598028dfd5b4e0950f504042f
Vineeth <96077560+vineethevk@users.noreply.github.com>
2022-01-20 13:06:50 +0530

    single product page update

---
+++ scripts/singleProduct.js
@@ -0,0 +1,306 @@
+
+// var arr = [
+//     {
+//         "name": "Zero Half Sleeves T-Shirt Striped - Navy",
+//         "price": "156.60",
+//         "image": "https://cdn.fcglcdn.com/brainbees/images/products/438x531/10393343a.webp",
+//         "image1": "https://cdn.fcglcdn.com/brainbees/images/products/438x531/10393343b.webp",
+//         "image2": "https://cdn.fcglcdn.com/brainbees/images/products/438x531/10393343c.webp",
+//         "image3": "https://cdn.fcglcdn.com/brainbees/images/products/438x531/10393343d.webp",
+//     }
+// ]
+// localStorage.setItem('single_product', JSON.stringify(arr))
+// var arr = [
+//     {
+//         "name": "Zero Half Sleeves T-Shirt Striped - Navy",
+//         "price": "156.60",
+//         "image": "https://cdn.fcglcdn.com/brainbees/images/products/438x531/10393343a.webp",
+//         "image1": "https://cdn.fcglcdn.com/brainbees/images/products/438x531/10393343b.webp",
+//         "image2": "https://cdn.fcglcdn.com/brainbees/images/products/438x531/10393343c.webp",
+//         "image3": "https://cdn.fcglcdn.com/brainbees/images/products/438x531/10393343c.webp",
+//     },
+//     {
+//         "name": "Ollypop Half Sleeves T-Shirt Text Print - Waterfall Blue",
+//         "price": "267.81",
+//         "image": "https://cdn.fcglcdn.com/brainbees/images/products/438x531/10387650a.webp",
+//         "image1": "https://cdn.fcglcdn.com/brainbees/images/products/438x531/10387650b.webp",
+//         "image2": "https://cdn.fcglcdn.com/brainbees/images/products/438x531/10387650c.webp",
+//         "image3": "https://cdn.fcglcdn.com/brainbees/images/products/438x531/10387650e.webp",
+//     },
+//     {
+//         "name": "Pine Kids Half Sleeves Biowashed Tee Rocket Print - Blue",
+//         "price": "1999",
+//         "image": "https://cdn.fcglcdn.com/brainbees/images/products/438x531/10249004a.webp",
+//         "image1": "https://cdn.fcglcdn.com/brainbees/images/products/438x531/10249004b.webp",
+//         "image2": "https://cdn.fcglcdn.com/brainbees/images/products/438x531/10249004c.webp",
+//         "image3": "https://cdn.fcglcdn.com/brainbees/images/products/438x531/10249004d.webp",
+//     },
+//     {
+//         "name": "Pine Kids Half Sleeves Biowash Tee Lion Print - Black",
+//         "price": "394.21",
+//         "image": "https://cdn.fcglcdn.com/brainbees/images/products/438x531/10184529a.webp",
+//         "image1": "https://cdn.fcglcdn.com/brainbees/images/products/438x531/10184529b.webp",
+//         "image2": "https://cdn.fcglcdn.com/brainbees/images/products/zoom/10184529c.jpg",
+//         "image3": "https://cdn.fcglcdn.com/brainbees/images/products/438x531/10184529d.webp",
+//     },
+//     {
+//         "name": "Ollypop Half Sleeves T-Shirt Text Print - Candy Pink",
+//         "price": "207.93",
+//         "image": "https://cdn.fcglcdn.com/brainbees/images/products/438x531/10387587a.webp",
+//         "image1": "https://cdn.fcglcdn.com/brainbees/images/products/zoom/10387587b.jpg",
+//         "image2": "https://cdn.fcglcdn.com/brainbees/images/products/438x531/10387587c.webp",
+//         "image3": "",
+//     },
+//     {
+//         "name": "Play by Little Kangaroos Half Sleeves T-Shirt - Red",
+//         "price": "413.31 ",
+//         "image": "https://cdn.fcglcdn.com/brainbees/images/products/438x531/9191970a.webp",
+//         "image1": "https://cdn.fcglcdn.com/brainbees/images/products/zoom/9191970b.jpg",
+//         "image2": "https://cdn.fcglcdn.com/brainbees/images/products/zoom/9191970c.jpg",
+//         "image3": "",
+//     },
+//     {
+//         "name": "Ollypop Half Sleeves T-Shirt Palm Trees & Island Print - Blue",
+//         "price": "213.31",
+//         "image": "https://cdn.fcglcdn.com/brainbees/images/products/438x531/10387619a.webp",
+//         "image1": "https://cdn.fcglcdn.com/brainbees/images/products/zoom/10387619b.jpg",
+//         "image2": "https://cdn.fcglcdn.com/brainbees/images/products/438x531/10387619c.webp",
+//         "image3": "https://cdn.fcglcdn.com/brainbees/images/products/zoom/10387619d.jpg",
+//     },
+//     {
+//         "name": "Taeko Half Sleeves T-Shirt Planes Print - Red",
+//         "price": "155.73 ",
+//         "image": "https://cdn.fcglcdn.com/brainbees/images/products/438x531/10382086a.webp",
+//         "image1": "https://cdn.fcglcdn.com/brainbees/images/products/zoom/10382086b.jpg",
+//         "image2": "https://cdn.fcglcdn.com/brainbees/images/products/438x531/10382086c.webp",
+//         "image3": "",
+//     },
+//     {
+//         "name": "Pine Kids Half Sleeves Bio Wash Tee Super Leaf Print - Blue",
+//         "price": "555.21",
+//         "image": "https://cdn.fcglcdn.com/brainbees/images/products/438x531/10183295a.webp",
+//         "image1": "https://cdn.fcglcdn.com/brainbees/images/products/zoom/10183295b.jpg",
+//         "image2": "",
+//         "image3": "",
+//     },
+//     {
+//         "name": "Polka Tots Full Sleeves Boys Rules Print Tee - Black",
+//         "price": "338.53",
+//         "image": "https://cdn.fcglcdn.com/brainbees/images/products/438x531/10525123a.webp",
+//         "image1": "https://cdn.fcglcdn.com/brainbees/images/products/zoom/10525123b.jpg",
+//         "image2": "https://cdn.fcglcdn.com/brainbees/images/products/438x531/10525123c.webp",
+//         "image3": "https://cdn.fcglcdn.com/brainbees/images/products/438x531/10525123sz.webp",
+//     }
+// ]
+
+localStorage.setItem("productList", JSON.stringify(arr))
+let data = JSON.parse(localStorage.getItem('single_product'));
+
+let short_btn = document.getElementById('shortlist')
+short_btn.addEventListener('onclick', short_list(data));
+function short_list(data) {
+    console.log(data);
+    let shortlist = JSON.parse(localStorage.getItem('shortlist')) || [];
+    shortlist.push(data);
+    localStorage.setItem('shortlist', JSON.stringify(shortlist));
+
+}
+function Display_img() {
+
+    console.log(data);
+
+    let div1 = document.createElement('div');
+    let img1 = document.createElement('img')
+    img1.src = data[0].image
+    div1.append(img1);
+    img1.addEventListener('click', right_img)
+
+    let div2 = document.createElement('div');
+    let img2 = document.createElement('img')
+    img2.src = data[0].image1
+    div2.append(img2);
+    img2.addEventListener('click', right_img)
+
+    let div3 = document.createElement('div');
+    let img3 = document.createElement('img')
+    img3.src = data[0].image2
+    div3.append(img3);
+    img3.addEventListener('click', right_img)
+
+    let div4 = document.createElement('div');
+    let img4 = document.createElement('img')
+    img4.src = data[0].image3
+    div4.append(img4);
+    img4.addEventListener('click', right_img)
+
+    let small_img = document.getElementById('small_img');
+
+    small_img.append(div1, div2, div3, div4)
+
+    let big_img = document.getElementById('big_img');
+    let right_img_show = document.createElement('img');
+    right_img_show.id = "right_img_show"
+    let lens = document.createElement('div');
+    lens.id = "lens"
+    let result = document.createElement('div')
+    result.id = "result"
+
+    right_img_show.src = img1.src;
+    big_img.append(right_img_show, lens, result);
+
+    function right_img(event) {
+
+        right_img_show.src = (event.srcElement.currentSrc);
+    }
+    // Zooming starts
+    let containerRect = big_img.getBoundingClientRect();
+    let imageRect = right_img_show.getBoundingClientRect();
+    let lensRect = lens.getBoundingClientRect();
+    let resultRect = result.getBoundingClientRect();
+
+    big_img.addEventListener("mousemove", zoomimg)
+
+    function mouse_out() {
+        // console.log("Vineeth")
+        result.innerHTML = null;
+    }
+
+
+
+    function zoomimg(e) {
+        // console.log(e.clientX, e.clientY)
+
+        // big_img.append(lens, result);
+        result.style.backgroundImage = `url(${right_img_show.src})`
+
+        let { x, y } = getmousepos(e);
+
+        lens.style.left = x + "px"
+        lens.style.top = y + "px"
+
+        let fx = resultRect.width / lensRect.width
+        let fy = resultRect.height / lensRect.height
+
+        // console.log(fx, fy)
+
+        result.style.backgroundSize = `${imageRect.width * fx}px ${imageRect.height * fy}px`
+
+        result.style.backgroundPosition = `-${x * fx}px -${y * fy}px  `
+
+
+
+    }
+    function getmousepos(e) {
+        let x = e.clientX - containerRect.left - lensRect.width / 2;
+        let y = e.clientY - containerRect.top - lensRect.height / 2;
+
+        let minX = 0;
+        let minY = 0;
+        let maxX = containerRect.width - lensRect.width
+        let maxY = containerRect.height - lensRect.height
+
+        if (x <= minX) {
+            x = minX;
+
+        }
+        else if (x >= maxX) {
+            mouse_out();
+        }
+
+        // if (y <= minY) {
+        //     y = minY;
+        // }else
+        if (y >= maxY) {
+            y = maxY;
+        }
+        // if ((x < minX) || (x > maxX) || (y < minY) || (y > maxY)) {
+        //     mouse_out();
+        // }
+
+        return { x, y }
+
+    }
+
+
+
+    let heading = document.getElementById('heading');
+    heading.innerText = data[0].name;
+
+    let price = document.getElementById('price');
+    price.innerHTML = "₹ " + data[0].price
+
+    // right.append(heading);
+}
+Display_img();
+function you_like() {
+    let data = JSON.parse(localStorage.getItem('productList'));
+
+    for (let i = 0; i < 5; i++) {
+        let name = data[i].name;
+        let price = data[i].price;
+        let image = data[i].image
+
+        let div = document.createElement('div');
+        let img_div = document.createElement('div')
+        img_div.className = "img_div"
+
+        let text_div = document.createElement('div')
+        text_div.className = "text_div"
+
+        let img = document.createElement('img')
+        img.src = image;
+
+        let title = document.createElement('p')
+        title.innerText = name;
+
+        let p = document.createElement('p')
+        p.innerText = "₹ " + price;
+
+        img_div.append(img);
+        text_div.append(title, p);
+
+        div.append(img_div, text_div);
+
+        let like = document.getElementById('like')
+
+        like.append(div);
+    }
+
+}
+you_like();
+function frequently() {
+    let data = JSON.parse(localStorage.getItem('productList'));
+
+    for (let i = 5; i < 10; i++) {
+        let name = data[i].name;
+        let price = data[i].price;
+        let image = data[i].image
+
+        let div = document.createElement('div');
+        let img_div = document.createElement('div')
+        img_div.className = "img_div"
+
+        let text_div = document.createElement('div')
+        text_div.className = "text_div"
+
+        let img = document.createElement('img')
+        img.src = image;
+
+        let title = document.createElement('p')
+        title.innerText = name;
+
+        let p = document.createElement('p')
+        p.innerText = "₹ " + price;
+
+        img_div.append(img);
+        text_div.append(title, p);
+
+        div.append(img_div, text_div);
+
+        let like = document.getElementById('freq')
+
+        like.append(div);
+    }
+
+}
+frequently();
