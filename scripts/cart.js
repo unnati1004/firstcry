69dc9a64267f5f572cb82970cb57a797b376c4b7
Sumitnarwal <sumitnarwal39@gmail.com>
2022-01-21 21:41:56 +0530

    cart update

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,27 +1,64 @@
-let local=JSON.parse(localStorage.getItem("Cart"))
-console.log(local)
-appenddata(local)
-function appenddata(local){
-local.forEach((elem)=>{
-let div=document.createElement("div")
-let imgdiv=document.createElement("div")
 
-let name=document.createElement("p")
-let price=document.createElement("p")
-price.textContent=elem.price
+var data = JSON.parse(localStorage.getItem("Cart"));
+var short = JSON.parse(localStorage.getItem("shortlist")) || [];
+console.log(data)
 
-let img=document.createElement("img")
-img.src=elem.image
+data.map((el, index) => {
 
-name.textContent=elem.name
+    let { name, image, price } = el;
+    var div = document.createElement("div");
+    var div1 = document.createElement("div");
+    var div2 = document.createElement("div");
+    var div3 = document.createElement("div");
 
-div.append(name,price)
-imgdiv.append(img)
-document.getElementById("itemsset").append(imgdiv,div)
-//document.getElementById("itemsimg").append(imgdiv)
-//document.getElementById("itemsname").append(div)
+    div.style.display = "flex"
+    div.style.border = "1px solid black"
+    var img = document.createElement("img");
+    img.src = image
+    img.setAttribute("id", "imgitems")
+    div1.append(img)
 
+    var nm = document.createElement("p");
+    nm.textContent = name;
+    var btt = document.createElement("button")
+    btt.textContent = "REMOVE"
+
+    var bttsl = document.createElement("button")
+    bttsl.textContent = "MOVE TO SHORTLIST"
+    bttsl.addEventListener("click", function () {
+        short.push(el)
+        localStorage.setItem("shortlist", JSON.stringify(short))
+        remove(el)
+    })
+
+    div3.append(btt, bttsl)
+    btt.addEventListener("click", function () {
+        remove(index)
+    })
+    var pr = document.createElement("p");
+    pr.textContent = "price:" + price;
+    div2.append(nm, pr)
+    div2.setAttribute("id", "div2")
+    div.append(div1, div2);
+
+    document.getElementById("itemsset").append(div, div3);
 
 
 })
+document.querySelector("#shopping_no").textContent = "(" + data.length + ")";
+
+var sum = 0;
+data.map(function (elem) {
+    let { price } = elem;
+    sum = sum + (+price)
+    //    document.querySelector("#ttp").innerHTML="Total price is "+ sum;
+})
+console.log(sum.toFixed(2));
+document.getElementById("price_show").textContent = "Rs " + sum;
+
+function remove(elm) {
+    data.splice(elm, 1)
+    localStorage.setItem("Cart", JSON.stringify(data))
+    location.reload()
 }
+
