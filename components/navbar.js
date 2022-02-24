ecead381c3217fd7b9c4f6f77b49be24fedc009c
Vineeth <96077560+vineethevk@users.noreply.github.com>
2022-02-25 00:49:54 +0530

    update

--- components/navbar.js
+++ components/navbar.js
@@ -28,7 +28,7 @@ function navbar() {
             <a href="./shortlist.html"><span>Shortlist</span></a>
          </div>
         <div  class="location" id="carticon">
-            <a href="./sumit.html"><svg viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" mr="8" class="Iconp"
+            <a href="./shtml"><svg viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" mr="8" class="Iconp"
             stroke="greyBase" iconsize="20">
             <path
                 d="M4.987 5.469l1.848 7.2a1 1 0 00.968.752h8.675a1 1 0 00.962-.726l1.697-5.952a1 1 0 00-.962-1.274H4.987zm0 0l-.943-3.248a1 1 0 00-.96-.721H1"
