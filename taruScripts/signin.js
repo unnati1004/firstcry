ccebeeafba33ba9d1be3a765997010c2b4563ba1
Vineeth <96077560+vineethevk@users.noreply.github.com>
2022-02-25 20:08:00 +0530

    signin & Login backed update

--- taruScripts/signin.js
+++ taruScripts/signin.js
@@ -1,21 +1,21 @@
-// document.getElementById("form").addEventListener("submit",signIn)
-// arr=JSON.parse(localStorage.getItem("signIn"))||[]
-// function signIn(event){
-// event.preventDefault()
-// // console.log("here");
-// myObj={
-// name:form.name.value,
-// mobile:form.mobile.value,
-// email:form.email.value,
-// password:form.password.value,
-// }
+// document.getElementById("form").addEventListener("submit", signIn)
+// arr = JSON.parse(localStorage.getItem("signIn")) || []
+// function signIn(event) {
+//     event.preventDefault()
+//     // console.log("here");
+//     myObj = {
+//         name: form.name.value,
+//         mobile: form.mobile.value,
+//         email: form.email.value,
+//         password: form.password.value,
+//     }
 
-// // console.log(myObj)
+//     // console.log(myObj)
 
-// arr.push(myObj)
-// localStorage.setItem("signIn",JSON.stringify(arr))
-// alert("thank you")
-// window.location.href="login.html"
+//     arr.push(myObj)
+//     localStorage.setItem("signIn", JSON.stringify(arr))
+//     alert("thank you")
+//     window.location.href = "login.html"
 // }
 
 document.getElementById("form").addEventListener("submit", signIn)
@@ -31,15 +31,25 @@ async function signIn(event) {
     myObj = JSON.stringify(myObj)
 
     try {
-        let response = await fetch('http://localhost:2345/register', {
+        let response = await fetch('https://firstcryserver.herokuapp.com/register', {
             method: 'POST',
             body: myObj,
             headers: {
                 "Content-Type": "application/json"
             }
         });
+        let data = await response.json();
+        if (data.message !== undefined) {
+            alert(data.message);
+        } else {
+            window.location.href = "login.html"
+        }
+
     } catch (e) {
-        console.log(e);
+        alert(e.message);
     }
+
+    // alert("thank you")
+    // window.location.href = "login.html"
 
 }
